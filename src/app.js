279d23d43e5906ee2d3e6f5e6cf6b5c80d922196
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-07 21:59:54 -0300

    cria a rota /livros

--- src/app.js
+++ src/app.js
@@ -4,8 +4,23 @@ const PORT = 3000;
 
 const app = express();
 
+const livros = [
+  {
+    id: 1,
+    titulo: "O Senhor dos Pastéis",
+  },
+  {
+    id: 2,
+    titulo: "O Hobbit",
+  },
+];
+
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
 });
 
 export default app;
