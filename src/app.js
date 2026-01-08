1ac79502c1d5700cb5b4bce7281f94fa478d828f
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-08 17:59:38 -0300

    cria o controller de deletar

--- src/app.js
+++ src/app.js
@@ -17,22 +17,6 @@ const PORT = 3000;
 const app = express();
 routes(app);
 
-app.get("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  res.status(200).json(livros[index]);
-});
-
-app.post("/livros", (req, res) => {
-  const novoLivro = livros.push(req.body);
-  res.status(201).send("livro cadastrado com sucesso!");
-});
-
-app.put("/livros/:id", (req, res) => {
-  const index = buscaLivro(req.params.id);
-  livros[index].titulo = req.body.titulo;
-  res.status(200).json(livros);
-});
-
 app.delete("/livros/:id", (req, res) => {
   const index = buscaLivro(req.params.id);
   livros.splice(index, 1);
--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -12,7 +12,7 @@ class LivroController {
     }
   }
 
-  static async listarLivro(req, res) {
+  static async listarLivroPorId(req, res) {
     try {
       const id = req.params.id;
       const livroEncontrado = await livro.findById(id);
@@ -32,6 +32,30 @@ class LivroController {
       res
         .status(500)
         .json({ message: `${error.message} - falha ao cadastrar livro.` });
+    }
+  }
+
+  static async atualizarLivroPorId(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "Livro atualizado com sucesso!" });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha ao atualizar o livro.` });
+    }
+  }
+
+  static async deletarLivroPorId(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndDelete(id);
+      res.status(200).json({ message: "Livro deletado com sucesso!" });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha ao deletar o livro.` });
     }
   }
 }
