05ca2307155b155424cd3b178b1a1cb467370f6b
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-07 21:47:17 -0300

    adaptando para usar o express

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-import http from "http";
+// import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
+/*const rotas = {
   "/": "Curso de Express API",
   "/livros": "Entrei na rota livros",
   "/autores": "Entrei na rota autores",
-};
+};*/
 
-const server = http.createServer((req, res) => {
+/*const server = http.createServer((req, res) => {
   res.writeHead(200, { "content-type": "text-plain" });
   res.end(rotas[req.url]);
-});
+});*/
 
+/*
 server.listen(PORT, () => {
   console.log("servidor escutando!");
+});
+*/
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
 });
