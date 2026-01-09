c971a1cd19051d5d4663bb6f59d8d81647ea361c
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-09 11:26:24 -0300

    cria a rota de busca  por parametro editora

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -67,6 +67,18 @@ class LivroController {
         .json({ message: `${error.message} - falha ao deletar o livro.` });
     }
   }
+
+  static async listarLivrosPorEditora(req, res) {
+    const editora = req.query.editora;
+    try {
+      const livrosPorEditora = await livro.find({ editora: editora });
+      res.status(200).json(livrosPorEditora);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha ao buscar editora.` });
+    }
+  }
 }
 
 export default LivroController;
