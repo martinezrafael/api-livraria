a1048895642ab42cef810eb40d9df3a4558da38d
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-09 17:30:31 -0300

    Cria arquivos especificos para os erros e separa as responsabilidades

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,19 +1,15 @@
 import mongoose, { mongo } from "mongoose";
+import ErroBase from "../erros/ErroBase.js";
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
+import ErroValidacao from "../erros/ErroValidacao.js";
 
 function manipuladorDeErros(erro, req, res, next) {
   if (erro instanceof mongoose.Error.CastError) {
-    res
-      .status(400)
-      .json({ message: "Um ou mais dados fornecidos estão incorretos." });
+    new RequisicaoIncorreta().enviarResposta(res);
   } else if (erro instanceof mongoose.Error.ValidationError) {
-    const mensagensErro = Object.values(erro.errors)
-      .map((erro) => erro.message)
-      .join("; ");
-    res.status(400).json({
-      message: `Os seguintes erros foram encontrados: ${mensagensErro}`,
-    });
+    new ErroValidacao(erro).enviarResposta(res);
   } else {
-    res.status(500).json({ message: `Erro interno de servidor.` });
+    new ErroBase().enviarResposta(res);
   }
 }
 
