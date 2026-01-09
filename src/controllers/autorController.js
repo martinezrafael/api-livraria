f5269135764c347ce4546a4e680eb54c56650ba0
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-09 17:54:05 -0300

    Tratando o erro de página 404

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -1,3 +1,4 @@
+import NaoEncontrado from "../erros/NaoEncontrado.js";
 import { autor } from "../models/Autor.js";
 
 class AutorController {
@@ -18,7 +19,7 @@ class AutorController {
       if (autorEncontrado !== null) {
         res.status(200).json(autorEncontrado);
       } else {
-        res.status(404).json({ message: "Id do autor não localizado." });
+        next(new NaoEncontrado("Id do autor não localizado."));
       }
     } catch (error) {
       next(error);
