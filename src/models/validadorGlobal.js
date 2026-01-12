6ed95a40311de62ad786f88a291ead36ad6a33d0
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-12 15:10:23 -0300

    cria o validadorGlobal

--- src/models/validadorGlobal.js
+++ src/models/validadorGlobal.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 mongoose.Schema.Types.String.set("validate", {
-  validator: (valor) => valor !== "",
-  message: ({ path }) => `O campo ${path} foi fornecido em  branco.`,
+  validator: (valor) => valor.trim() !== "",
+  message: ({ path }) => `O campo ${path} foi fornecido em branco.`,
 });
