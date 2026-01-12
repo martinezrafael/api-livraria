d144582f2fec8e6744af20b8168bb91394a337a5
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-12 14:49:23 -0300

    modifica a validação do numero min e max de paginas no model livro

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -18,6 +18,14 @@ const livroSchema = new mongoose.Schema(
     preco: { type: Number },
     paginas: {
       type: Number,
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message:
+          "O número de páginas deve estar entre 10 e 5.000. Valor fornecido: {VALUE}",
+      },
+      /*
       min: [
         10,
         "O número de páginas deve estar entre 10 e 5.000. Valor fornecido: {VALUE}",
@@ -26,6 +34,7 @@ const livroSchema = new mongoose.Schema(
         5000,
         "O número de páginas deve estar entre 10 e 5.000. Valor fornecido: {VALUE}",
       ],
+      */
     },
     autor: {
       type: mongoose.Schema.Types.ObjectId,
