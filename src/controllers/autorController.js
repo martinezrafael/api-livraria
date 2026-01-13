29ba41446a45507df19408214cb8ff6a2689f117
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-01-13 16:26:00 -0300

    replica o middleware de paginacao para outras rotas

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -4,8 +4,9 @@ import { autor } from "../models/index.js";
 class AutorController {
   static listarAutores = async (req, res, next) => {
     try {
-      const listaAutores = await autor.find();
-      res.status(200).json(listaAutores);
+      const listaAutores = autor.find();
+      req.resultado = listaAutores;
+      next();
     } catch (error) {
       next(error);
     }
--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,27 +1,11 @@
-import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 import { livro, autor } from "../models/index.js";
 
 class LivroController {
   static listarLivros = async (req, res, next) => {
     try {
-      let { limite = 5, pagina = 1, ordenacao = "_id:-1'" } = req.query;
-
-      let [campoOrdenacao, ordem] = ordenacao.split(":");
-
-      limite = parseInt(limite);
-      pagina = parseInt(pagina);
-      ordem = parseInt(ordem);
-
-      if (limite > 0 && pagina > 0) {
-        const listaLivros = await livro
-          .find()
-          .sort({ [campoOrdenacao]: ordem })
-          .skip((pagina - 1) * limite)
-          .limit(limite);
-        res.status(200).json(listaLivros);
-      } else {
-        next(new RequisicaoIncorreta());
-      }
+      const buscaLivros = livro.find();
+      req.resultado = buscaLivros;
+      next();
     } catch (error) {
       next(error);
     }
@@ -79,8 +63,9 @@ class LivroController {
       const busca = await processaBusca(req.query);
 
       if (busca !== null) {
-        const livrosPorEditora = await livro.find(busca).populate("autor");
-        res.status(200).json(livrosPorEditora);
+        const livrosResultado = livro.find(busca).populate("autor");
+        req.resultado = livrosResultado;
+        next();
       } else {
         res.status(200).json([]);
       }
