fddfe4b52229d267bcb703ce09e6ca2817d85f5f
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-08-18 16:26:13 -0300

    criando a rota post para livros

--- src/app.js
+++ src/app.js
@@ -1,13 +1,34 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+const livros = [
+    {
+        id: 1,
+        titulo: "Senhor dos Anéis",
+        autor: "J.R.R. Tolkien",
+        lancamento: 1954,
+    },
+    {
+        id: 2,
+        titulo: "O Hobbit",
+        autor: "J.R.R. Tolkien",
+        lancamento: 1937,
+    },
+]
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js com Express.");
 })
 
 app.get("/livros", (req, res) => {
-    res.status(200).send("Entrei na rota de livros.");
+    res.status(200).json(livros);
+})
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso!");
 })
 
 export default app;
