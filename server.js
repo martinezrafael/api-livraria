f4161bd54fd17ac71be22a4ec0502432e94f6276
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-08-18 15:07:57 -0300

    Cria as primeiras rotas e instala o nodemon

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+    "/": "Curso Express API",
+    "/livros": "Entrei na rota de livros",
+    "/autores": "Entrei na rota de autores",
+    "/editoras": "Entrei na rota de editoras",
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Curso de Node.js");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
