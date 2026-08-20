01a5534e9291a58d2c62b9e3755a05eb600de2fd
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-08-20 16:04:41 -0300

    Criando o livro referenciando o autor por id

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import livros from "./livroRoutes.js";
-import autores from "./autorRoutes.js"
+import autores from "./autorRoutes.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => res.status(200).send("Curso de Node.js"));
