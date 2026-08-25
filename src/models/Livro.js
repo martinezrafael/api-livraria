d85194ef930d3a479925a84b4a26a8d619f694ee
martinezrafael <rafaelmartinez.contato@gmail.com>
2026-08-25 09:35:29 -0300

    Adiciona numero minimo e máximo de páginas para livro

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -9,7 +9,11 @@ const livroSchema = new mongoose.Schema(
       required: [true, "O título do livro é obrigatório."] },
     editora: { type: String, required: [true, "A editora é obrigatória"] },
     preco: { type: Number },
-    paginas: { type: Number },
+    paginas: { 
+      type: Number,
+      min: [10, "O número de páginas deve estar entre 10 e 5000. Número fornecido: {VALUE}"],
+      max: [5000, "O número de páginas deve estar entre 10 e 5000. Número fornecido: {VALUE}"] 
+    },
     autor: autorSchema
   },
   { versionKey: false },
